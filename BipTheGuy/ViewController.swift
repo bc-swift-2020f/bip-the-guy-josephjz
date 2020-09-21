d1b9e4d18c53ab7951dd415a11241aaea82e19fd
Jennifer Joseph <josephjz@bc.edu>
2020-09-21 16:15:50 -0400

    Progress on Bip the Guy App

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -31,21 +31,14 @@ class ViewController: UIViewController {
         }
     }
 
-    @IBAction func punchButtonPressed(_ sender: UIButton) {
+    @IBAction func photoOrCameraPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert Title Goes Here", message: "I think you are awesome!" , preferredStyle: .alert)
         
-        let originalImageFrame = imageView.frame
-        let imageWidthShrink : CGFloat = 20
-        let imageHeightShrink : CGFloat = 20
-        let smallerImageFrame = CGRect(x: imageView.frame.origin.x + imageWidthShrink, y: imageView.frame.origin.y + imageHeightShrink , width: imageView.frame.width - (imageWidthShrink * 2), height: imageView.frame.height - (imageHeightShrink*2))
-        
-        imageView.frame = smallerImageFrame
-        
-        playSound(soundName: "punchSound")
-        
-        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0, options: [], animations: {self.imageView.frame = originalImageFrame })
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
     
-
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         let originalImageFrame = imageView.frame
         let imageWidthShrink : CGFloat = 20
